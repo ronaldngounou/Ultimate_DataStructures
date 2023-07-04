7cab4575645c11218310c928212682303c96f924
ronaldngounou <landryzinkoue@yahoo.fr>
2023-07-04 21:38:29 +0200

    trees are equals

--- Data Structures - Part 2/MyCodes-Part2/Part2-BinaryTrees_AVLTrees_Heaps_TriesGraphs/src/com/datastructures/Main.java
+++ Data Structures - Part 2/MyCodes-Part2/Part2-BinaryTrees_AVLTrees_Heaps_TriesGraphs/src/com/datastructures/Main.java
@@ -17,6 +17,16 @@ public class Main {
         tree.insert(10);
         tree.find(10);
 
+        Tree tree2 = new Tree();
+        tree2.insert(7);
+        tree2.insert(4);
+        tree2.insert(9);
+        tree2.insert(1);
+        tree2.insert(6);
+        tree2.insert(8);
+        tree2.insert(10);
+        tree2.find(10);
+
         // Tree Traversals
          /*Breadth First (Level order traversal)
          Depth First
@@ -45,6 +55,13 @@ public class Main {
         // tree.traversePreOrder();
         // tree.traverseInOrder();
         //tree.traversePostOrder();
+        //System.out.println(tree.height());
+        //System.out.println(tree.min());
+
+        // Interview question
+        System.out.println(tree.equals(null));
+
+
 
     }
 
--- Data Structures - Part 2/MyCodes-Part2/Part2-BinaryTrees_AVLTrees_Heaps_TriesGraphs/src/com/datastructures/Tree.java
+++ Data Structures - Part 2/MyCodes-Part2/Part2-BinaryTrees_AVLTrees_Heaps_TriesGraphs/src/com/datastructures/Tree.java
@@ -107,6 +107,62 @@ public class Tree {
         System.out.println(root);
     }
 
+    public int height(){ return height(root); }
+
+    private int height(Node root){
+        if (root==null)
+            return -1;
+
+        if (root.leftChild == null && root.rightChild == null)
+            return 0;
+
+        return 1 + Math.max(height(root.leftChild), height(root.rightChild));
+    }
+
+    // Find the minimum value in a binary search tree.
+    // O(log n) because we are narrowing down our search.
+    public int min(){
+        if (root==null)
+            throw new IllegalStateException();
+
+        var current = root;
+        var last = current;
+        while (current != null){
+            last = current;
+            current = current.leftChild;
+        }
+        return last.value;
+    }
+
+    // O(n) because we have to traverse every node to find the min value.
+    // Find the minimum value in a binary tree
+    private int min(Node root){
+        if (root.leftChild == null && root.rightChild == null) //isLeaf
+            return root.value; //when we get to a leaf node we return the value of that node itself.
+
+        var left = min(root.leftChild);
+        var right = min(root.rightChild);
+
+        return Math.min(Math.min(left, right), root.value);
+    }
+
+    public boolean equals(Tree other){
+        if (other == null)
+            return false;
+        return equals(root, other.root);
+    }
+
+    private boolean equals(Node first, Node second){
+        if (first == null && second == null)
+            return true;
+
+        if (first != null && second != null)
+            return first.value == second.value                     //pre order traversal: root left right
+                    && equals(first.leftChild, second.leftChild)
+                    && equals(first.rightChild, second.rightChild);
+
+        return false;
+    }
 
 
 }
