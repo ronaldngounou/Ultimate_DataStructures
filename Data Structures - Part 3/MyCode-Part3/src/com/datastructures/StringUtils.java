cac0652a849823938bbaf6cc36e6845f0ef1fbf2
ronaldngounou <landryzinkoue@yahoo.fr>
2023-07-12 18:43:23 +0200

    reverse string

--- Data Structures - Part 3/MyCode-Part3/src/com/datastructures/StringUtils.java
+++ Data Structures - Part 3/MyCode-Part3/src/com/datastructures/StringUtils.java
@@ -15,4 +15,30 @@ public class StringUtils {
         return  count;
     }
 
+    public static String reverse(String str){
+        if (str == null)
+            return "";
+
+        // String reversed = "";
+        // StringBuilder doesn't recreate a new string each time we append a char.
+        StringBuilder reversed = new StringBuilder();
+        for (var i = str.length() - 1; i >= 0; i--) // O(n)
+            //reversed += str.charAt(i); // O(n)
+            reversed.append(str.charAt(i));
+
+        return reversed.toString();
+    }
+
+    public static String reverseWords(String sentence){
+        //split the sentece in individual words
+        String[] words = sentence.split(" ");
+        StringBuilder reversed = new StringBuilder();
+
+        for (var i = words.length - 1; i >= 0; i--)
+            reversed.append(words[i] + " ");
+
+        return reversed.toString().trim();
+
+    }
+
 }
